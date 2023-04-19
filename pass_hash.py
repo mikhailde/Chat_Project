586bef211cfac4694e5a8e7317c260759d096d14
GoldenEagle74 <mshchannikov@yandex.ru>
2023-04-19 21:20:05 +0300

    Временно убрал из гита

--- pass_hash.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    salt = b'some_random_salt'
-    # Добавление соли к паролю
-    salted_password = password.encode('utf-8') + salt
-    # Хэширование пароля
-    hashed_password = hashlib.sha256(salted_password).hexdigest()
-    return hashed_password
