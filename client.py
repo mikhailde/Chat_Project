7509f8a7b8554e49096053befbb5d307ffa6e7b6
GoldenEagle74 <mshchannikov@yandex.ru>
2023-04-20 12:44:16 +0300

    Добавил комментари и методы приема и отправки сообщений

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
+"""Шифрование, сокеты и UI"""
+import socket
+import ssl
+
+from threading import Thread
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt6 import QtCore
 from UI import login, registration, ChatBox
-from threading import Thread
-
-import socket
-import ssl
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,30 +13,34 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_verify_locations(cafile='TLS/server.crt')
 serv = context.wrap_socket(sock, server_hostname='chatbox.ru')
-try: serv.connect(('localhost', 25565))
-except: exit()
+try:
+    serv.connect(('185.107.237.242', 25565))
+except Exception as e:
+    print(e)
 
 class Login(QWidget, login.Ui_ChatBox):
+    """Окно входа"""
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.label_2.installEventFilter(self)
         self.pushButton.clicked.connect(self.login)
         self.frame.setStyleSheet(stylesheet)
-        
+
 
     def login(self):
-        serv.send(':'.join(['login', self.lineEdit.text(), self.lineEdit_2.text()]).encode())
-        status = serv.recv(1024).decode()
-        print(status)
-        if status == 'Error': self.label_3.setText('Неверные данные')
+        """Метод входа"""
+        serv.sendall(':'.join(['login', self.lineEdit.text(), self.lineEdit_2.text()]).encode())
+        status = serv.recv(1024)
+        if status.decode() == 'Error':
+            self.label_3.setText('Неверные данные')
         else:
-            serv.send(':'.join(['online', self.lineEdit.text()]).encode())
+            serv.sendall(':'.join(['online', self.lineEdit.text()]).encode())
             self.close()
             main_window.show()
 
     def eventFilter(self, a0, a1) -> bool:
-        
+        """Захват нажатия на кнопку регистрации"""
         if a1.type() == 2:
             btn = a1.button()
             if btn == QtCore.Qt.MouseButton.LeftButton:
@@ -46,6 +51,7 @@ class Login(QWidget, login.Ui_ChatBox):
 
 
 class Registration(QWidget, registration.Ui_ChatBox):
+    """Окно регистрации"""
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -54,92 +60,86 @@ class Registration(QWidget, registration.Ui_ChatBox):
         self.frame.setStyleSheet(stylesheet)
 
     def registration(self):
-        if self.lineEdit_2.text() != self.lineEdit_3.text(): self.label_3.setText('Пароли не совпадают')
+        """Метод регистрации"""
+        if self.lineEdit_2.text() != self.lineEdit_3.text():
+            self.label_3.setText('Пароли не совпадают')
         else:
-            serv.send(':'.join(['register', self.lineEdit.text(), self.lineEdit_2.text()]).encode())
+            serv.sendall(':'.join(['register', self.lineEdit.text(), self.lineEdit_2.text()]).encode())
             status = serv.recv(1024).decode()
             print(status)
-            if status == 'Error': self.label_3.setText('Ошибка')
-            if status == 'Exists': self.label_3.setText('Уже зарегистрирован')
+            if status == 'Error':
+                self.label_3.setText('Ошибка')
+            if status == 'Exists':
+                self.label_3.setText('Уже зарегистрирован')
             else:
-                serv.send(':'.join(['online', self.lineEdit.text()]).encode())
+                serv.sendall(':'.join(['online', self.lineEdit.text()]).encode())
                 self.close()
                 main_window.show()
 
     def eventFilter(self, a0, a1) -> bool:
-        
+        """Захват нажатия на кнопку входа"""
         if a1.type() == 2:
             btn = a1.button()
             if btn == QtCore.Qt.MouseButton.LeftButton:
                 self.close()
                 login_window.show()
         return super().eventFilter(a0, a1)
-    
-def message():
-    while True:
-            data = serv.recv(1024).decode().split(':')
-            operation, *message = data
-            if operation == 'online':
-                count, *users = message
-                main_window.label.setText("Участники: {} в сети".format(count))
-                main_window.textEdit.setText('\n'.join(users))
-
 
 
 class MainWindow(QMainWindow, ChatBox.Ui_MainWindow):
+    """Окно чата"""
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.action_3.triggered.connect(self.close)
+        self.action_2.triggered.connect(self.logout)
         self.textBrowser.textChanged.connect(self.change)
-        # self.changeText = 
-        
+        self.pushButton_2.clicked.connect(self.send_message)
+        self.textEdit_2.toHtml()
+
+    def logout(self):
+        """Метод выхода из аккаунта"""
+        self.close()
+        login_window.show()
+
+    def send_message(self):
+        """Метод отправки сообщения"""
+        if self.lineEdit_3.text:
+            serv.sendall(':'.join(['message', self.lineEdit_3.text()]).encode())
+            self.textEdit_2.append('')
+            self.textEdit_2.insertHtml(f'<p align="right" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:11pt;">{self.lineEdit_3.text()}</span></p>')
+            self.textEdit_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+            self.lineEdit_3.clear()
+
     def change(self):
+        """Метод изменения списка участников"""
         self.textEdit.setText('\n'.join(self.users))
 
     def showEvent(self, event) -> None:
-        
-        th = Thread(target=self.message, daemon=True).start()
+        """Метод запускающий метод приема сообщений при запуске приложения"""
+        Thread(target=self.message, daemon=True).start()
+
+    def keyPressEvent(self, e) -> None:
+        """Метода захвата нажатия на Enter"""
+        if e.key() == 16777220:
+            self.send_message()
+        return super().keyPressEvent(e)
 
     def message(self):
+        """Метод приема сообщений"""
         while True:
             data = serv.recv(1024).decode().split(':')
             operation, *message = data
             if operation == 'online':
-
                 count, *users = message
                 self.users = users
                 self.label.setText("Участники: {} в сети".format(count))
                 self.textBrowser.append('123')
-
-
-class Message(QtCore.QThread):
-
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        while True:
-            data = serv.recv(1024).decode().split(':')
-            operation, *message = data
-            if operation == 'online':
-                count, *users = message
-                main_window.label.setText("Участники: {} в сети".format(count))
-                # main_window.textEdit.setText('\n'.join(users))
-
-
-    def stoped(self): 
-        return self.working
-        
-        
-    
-        
-
-    
-    # def message(self):
-    #     # serv.send(b'ready')
-        
-        
+            if operation == 'message':
+                username, message = message
+                self.textEdit_2.append('')
+                self.textEdit_2.insertHtml(f'<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:11pt; font-weight:600;">{username}:</span><span style=" font-size:11pt;"> {message}</span></p>')
+                self.textEdit_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
 
 
 stylesheet = """
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from threading import Thread
-import database as db
-
+"""Модули для захвата сигналов, сокетов, шифрования, потоков, и базы данных"""
+import signal
 import socket
 import ssl
+
+from threading import Thread
+
+import database as db
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -12,41 +15,58 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(certfile='TLS/server.crt', keyfile='TLS/server.key')
     ssl_sock = context.wrap_socket(sock, server_side=True)
+    print('Server started')
     clients = set()
 
 
-    def client_conn(conn):
+    def client_conn(conn, addr):
+        """Функция подключения клиента"""
         global clients
         print('Connected')
         with conn:
             while True:
                 data = conn.recv(1024).decode().split(':')
-                if data != ['']: operation, *message = data
+                if data != ['']:
+                    operation, *message = data
                 else:
-                    
-                    clients.remove((conn,username))
-                    for client in clients: client[0].send(':'.join(['online', str(len(clients)), ':'.join([client[1] for client in clients])]).encode())
-
+                    try:
+                        clients.remove((conn,username))
+                        for client in clients:
+                            client[0].sendall(':'.join(['online', str(len(clients)), ':'.join([client[1] for client in clients])]).encode())
+                    except Exception as e:
+                        print(e)
+                    print(f'Disconnected {addr}')
                     break
                 if operation == 'register':
-                    username, password = message
-                    if db.register(*message): conn.send(b'Success')
-                    else: conn.send('Exists'.encode())
+                    username = message[0]
+                    if db.register(*message):
+                        conn.sendall(b'Success')
+                    else:
+                        conn.sendall(b'Exists')
                 if operation == 'login':
-                    username, password = message
+                    username = message[0]
                     if db.login(*message):
-                        conn.send(b'Success')
-                    else: conn.send('Error'.encode())
+                        conn.sendall(b'Success')
+                    else: conn.sendall('Error'.encode())
                 if operation == 'online':
                     clients.add((conn, *message))
-                    for client in clients: client[0].send(':'.join(['online', str(len(clients)), ':'.join([client[1] for client in clients])]).encode())
+                    for client in clients:
+                        client[0].sendall(':'.join(['online', str(len(clients)), ':'.join([client[1] for client in clients])]).encode())
+                if operation == 'message':
+                    for client in clients:
+                        if client[0] != conn:
+                            client[0].sendall(':'.join(['message', username, *message]).encode())
 
-                    
-                # if operation == 'message':
-                
+    def stop_server(sig, frame):
+        """Метод остановки сервера"""
+        print('Server stopped')
+        clients.clear()
+        sock.close()
+        exit()
 
-
+    signal.signal(signal.SIGINT, stop_server)
 
     while True:
         conn, addr = ssl_sock.accept()
-        client_thread = Thread(target=client_conn, args=[conn], daemon=True).start()
+        print(f'Connected {addr}')
+        Thread(target=client_conn, args=[conn, addr], daemon=True).start()
